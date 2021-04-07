5e85aa2a52e2ccfcdca036ad8236ead7d9e8c0ae
Maximilian Dazer <maximilian.dazer@gmx.de>
2021-04-07 13:31:30 +0000

    added javascript for book modal

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
     $('.sidenav').sidenav({ edge: "right" });
 });
+
+// Add book modal
+function startModalBooks() {
+    const modal = document.querySelector(".container-modal")
+    modal.classList.add("show-modal");
+
+    modal.addEventListener("click", (e) => {
+        if (e.target.id == "modal-review" || e.target.className == "close-modal") {
+            modal.classList.remove("show-modal")
+        }
+    })
+}
+
+// Event Listener that starts the add book modal
+document.addEventListener('DOMContentLoaded', function () {
+    const addBook = document.getElementById("book-add")
+    if (addBook) {
+        addBook.addEventListener("click", () => {
+            startModalBooks()
+        })
+    }
+})
