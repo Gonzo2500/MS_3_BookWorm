73872ded98d420ed4d4ec00a93471a7706a84f6a
Maximilian Dazer <maximilian.dazer@gmx.de>
2021-04-10 11:44:16 +0000

    added code for delete and edit modal

--- static/js/script.js
+++ static/js/script.js
@@ -23,3 +23,45 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 })
+
+// Event Listener that starts the add book modal
+document.addEventListener('DOMContentLoaded', function () {
+    const addBook = document.getElementById("add-book")
+    if (addBook) {
+        addBook.addEventListener("click", () => {
+            startModalBooks()
+        })
+    }
+})
+
+// Event Listener that starts the edit book modal
+document.addEventListener('DOMContentLoaded', function () {
+    const editBook = document.getElementById("edit-book")
+    if (editBook) {
+        editBook.addEventListener("click", () => {
+            startModalBooks()
+        })
+    }
+})
+
+// Delete Modal
+function deleteModal() {
+    const modal = document.querySelector(".delete")
+    modal.classList.add("show-modal");
+
+    modal.addEventListener("click", (e) => {
+        if (e.target.id == "modal-delete" || e.target.className == "close-delete-modal" || e.target.id == "cancel-delete") {
+            modal.classList.remove("show-modal")
+        }
+    })
+}
+
+// Event Listener that starts delete modal
+document.addEventListener('DOMContentLoaded', function () {
+    const deleteBook = document.getElementById("delete-book")
+    if (deleteBook) {
+        deleteBook.addEventListener("click", () => {
+            deleteModal()
+        })
+    }
+})
