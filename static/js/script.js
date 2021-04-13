e857883e1bd43adf5c48902a5854ea2a421ab70a
Maximilian Dazer <maximilian.dazer@gmx.de>
2021-04-13 07:06:01 +0000

    dynamic addition of input fields

--- static/js/script.js
+++ static/js/script.js
@@ -89,3 +89,25 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }
 })
+
+// Function to dynamically add input fields
+//https://www.sanwebe.com/2013/03/addremove-input-fields-dynamically-with-jquery
+$(document).ready(function () {
+    let maxFields = 3; //maximum input boxes allowed
+    let addButton = $(".add-field-button"); //Add button ID
+    let inputs = $(".container-inputs")
+
+    let x = 1; //initlal text box count
+    $(addButton).click(function (e) { //on add input button click
+        console.log('clicked')
+        e.preventDefault();
+        if (x < maxFields) { //max input box allowed
+            x++; //text box increment
+            $(inputs).append('<div class="input-field col s12 add-vendor"><label for="vendor_url">Vendor URL:</label><input id="vendor_url" name="vendor_url" type="text" class="validate" minlength="5" maxlength="100" required><a href="#" class="remove_field btn-small">Remove</a></div>'); //add input box
+        }
+    });
+
+    $(inputs).on("click", ".remove_field", function (e) { //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
