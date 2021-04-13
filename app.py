d010120372f6379f5c1be49a7e9207c81b828b0a
Maximilian Dazer <maximilian.dazer@gmx.de>
2021-04-13 06:27:11 +0000

    added 500 error page

--- app.py
+++ app.py
@@ -295,7 +295,11 @@ def log_out():
 def not_found_error(error):
     return render_template('404.html', error=error), 404
 
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html', error=error), 500
 
+    
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
